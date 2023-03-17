6c331f4f49115549a5c39090db5e2677de6e2d91
PabloGarciaCaldero <pablo-.maria.garcia@estudiantat.upc.edu>
2023-03-17 14:00:37 +0100

    boton DeleteAllUsers

---
+++ src/controllers/employee.controller.js
@@ -0,0 +1,45 @@
+const { filter } = require("mongodb/lib/core/connection/logger");
+const Employee = require("../models/employee");
+const employee = require("../models/employee");
+
+const employeeCtrl = {};
+
+employeeCtrl.getEmployees = async (req, res, next) => {
+  const employees = await Employee.find();
+  res.json(employees);
+};
+
+employeeCtrl.createEmployee = async (req, res, next) => {
+  const employee = new Employee({
+    name: req.body.name,
+    position: req.body.position,
+    office: req.body.office,
+    salary: req.body.salary,
+  });
+  await employee.save();
+  res.json({ status: "Employee created" });
+};
+
+employeeCtrl.getEmployee = async (req, res, next) => {
+  const { id } = req.params;
+  const employee = await Employee.findById(id);
+  res.json(employee);
+};
+
+employeeCtrl.editEmployee = async (req, res, next) => {
+  const { id } = req.params;
+  await Employee.findByIdAndUpdate(id, {$set: req.body}, {new: true});
+  res.json({ status: "Employee Updated" });
+};
+
+employeeCtrl.deleteEmployee = async (req, res, next) => {
+  await Employee.findByIdAndRemove(req.params.id);
+  res.json({ status: "Employee Deleted" });
+};
+
+employeeCtrl.deleteAllEmployees = async (req, res, next) => {
+  await Employee.deleteMany();
+  res.json({status:"All Employees Deleted"})
+}
+
+module.exports = employeeCtrl;
